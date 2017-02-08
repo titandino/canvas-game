a37ef604c855f14ab1ec903eecd7b6669c2cd70d
Trenton Kress <trenton.kress@gmail.com>
2017-02-07 20:44:36 -0800

    Organize all the project files into an app folder and add some loaders to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
+const HTMLPlugin = require('html-webpack-plugin');
+
+const plugins = [
+  new HTMLPlugin({
+    template: './app/index.html',
+  })
+];
+
 module.exports = {
-  entry: './index',
+  entry: './app/index',
   output: {
     filename:'compiled.js',
     path:'./build'
+  },
+  plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css',
+      }
+    ],
   }
 };
