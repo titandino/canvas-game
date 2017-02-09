e2428de4a3f736619d88fa0c920394afedaea32a
Trenton Kress <trenton.kress@gmail.com>
2017-02-08 19:42:55 -0800

    Remove a few debug logs that were being used to figure out the stupid module require problem.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
 let Game = require('./game/engine/game');
 let StartMenu = require('./game/startmenu');
 
-console.log(StartMenu);
-console.log(require('./game/startmenu'));
 module.exports = new Game(new StartMenu());
